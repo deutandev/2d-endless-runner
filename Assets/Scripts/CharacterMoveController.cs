e45c3c853875383981b5c397c0bbb0ab1a6be7b1
Andika Fathin Azfar <deutandev@pm.me>
2021-09-15 22:01:17 +0700

    Player jumping

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -8,6 +8,13 @@ public class CharacterMoveController : MonoBehaviour
     public float moveAccel;
     public float maxSpeed;
 
+    // added in "Jumping"
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+
     private Rigidbody2D rig;
 
     // Start is called before the first frame update
@@ -19,7 +26,15 @@ public class CharacterMoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Added in "Jumping"
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+            }
+        }
     }
 
     void FixedUpdate()
